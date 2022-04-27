07ad05c57ba24180c1882369093708ab5ac90cfe
Minsoo-Shin <alstn5038@gmail.com>
2022-04-27 11:29:12 +0900

    feat : create todos 구현

--- database/sql.js
+++ database/sql.js
@@ -24,12 +24,21 @@ const sql = {
   },
 
   createTodo : async (user_id, content, completed) => {
-    return await promisePool.query(`
+    console.log(user_id, content, completed)
+    // 해당 INSERT가 완료하고 해당값 반환
+    const result = await promisePool.query(`
       INSERT INTO todos
       (name, completed, fk_user_id)
       VALUES
-      (${content}, ${completed}, ${user_id})
+      ('${content}', ${completed}, ${user_id})
       `)
+    insertId = result[0].insertId
+    const insertInfo = await promisePool.query(`
+      SELECT fk_user_id, name, completed, completed_at, created_at, updated_at
+      FROM todos
+      WHERE id = ${insertId};
+    `)
+    return insertInfo[0];
   },
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var sql = require('../database/sql');
-var authUser = require('./middles');
+var authUser = require('./middles')
+
 
 /* GET home page. */
 router.get('/', authUser, function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* create todos */ 
+router.post('/todos', authUser, async function(req, res, next) {
+  console.log(req.body)
+  const result = await sql.createTodo(
+    req.query.apikey,
+    req.body.name,
+    req.body.completed,
+  )
+  console.log('===',result[0])
+  res.send(result)
 });
 
 module.exports = router;
